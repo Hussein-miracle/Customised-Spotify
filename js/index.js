7e5b9e19fb7b1f52d102733530b3f1f390712d2c
Hussein <miraacle64@gmail.com>
2021-12-16 01:02:34 +0100

    finished the homepage,implemented the liked songs interface

--- js/index.js
+++ js/index.js
@@ -1,54 +1,189 @@
-"use strict";
+// "use strict";
+//configs 
+
+const musicTitleMax = 16;
+
+
+//
+
 const body = document.querySelector(".body")
 const openMenuBtn = document.querySelector(".menu")
+const closeMenuBtn = document.querySelector("#menu__close")
 const menuIcon = document.querySelector(".menu__icon");
 
-let isCloseRendered = false;
+const music = document.getElementById("music");
 
-function renderCloseIcon(){
-    
-    const markup1 = `<use id="menu__close" xlink:href ="./icons/sprite.svg#icon-arrow-left"></use>`;
-    const markup2 = `<use  xlink:href ="./icons/sprite.svg#icon-menu"></use>`;
+const musicTitle = document.querySelector(".music-player__content--title");
 
-    const markup = isCloseRendered ? markup1 : markup2; 
-    // console.log(isCloseRendered);
+const artistName = document.querySelector(".music-player__content--artist-name")
 
-    menuIcon.innerHTML =`${markup}`;
+const playBtn = document.querySelector(".music-btn__play");
 
+const nextBtn = document.querySelector(".music-btn__forward");
+const prevBtn = document.querySelector(".music-btn__backward");
 
-    // menuIcon.insertAdjacentHTML("afterbegin",markup);
-    // const closeMenuBtn = document.querySelector("#menu__close");
-    // console.log(closeMenuBtn);
-    // closeMenuBtn.addEventListener("click",() => {
-    //     body.classList.remove("show-nav");
-    //     // isRendered = false;
-    // })
+const musicCover = document.querySelector(".music-player__content-img")
 
+const btnContainer  = playBtn.querySelector("svg");
+
+const progressBar = document.querySelector(".progress-bar");
+
+const progressBarContainer = document.querySelector(".progress")
+
+//datas
+
+const songTitles = ["Love_Nwantinti","Young_Dumb_Broke","Baby-Riddim","Industry-Baby","Habibi","Montero-Call-Me-By-Your-Name"];
+
+const artists = ["Ckay","Khalid","Fave","Lil-Nas-X-Ft-Jack-Harlow","Ricky Rich FT ARAM Mafia","Lil-Nas-X"];
+
+let musicPlay;
+let index = 0;
+
+function renderSong(title){
+    music.src = `music/${title}.mp3`;
+    music.alt = `${artists[index]}-${title}`;
+    musicCover.src = `images/${title}.jpg`;
+
+    if(songTitles[index].length > musicTitleMax){
+        musicTitle.style.fontSize = "8px";
+    }else{
+        musicTitle.style.fontSize = "52.5%";
+    }
+    musicTitle.innerText= `${songTitles[index]}`;
+    artistName.innerText = `${artists[index]}`;
+
+}
+
+renderSong(songTitles[index],artists[index]);
+
+function renderPauseBtn(){
+    const markup = `
+        <svg  class="music-control__pause icon-size-1 icon-white-2">
+            <use xlink:href="./icons/sprite.svg#icon-pause">
+            </use>
+        </svg>
+                    `;    
+
+    playBtn.innerHTML = "";
+    playBtn.insertAdjacentHTML("afterbegin",markup)
 
 
 }
 
+function renderPlayBtn(){
+    const markup = `
+        <svg class="music-control__play icon-size-1 icon-white-2">
+            <use xlink:href="./icons/sprite.svg#icon-play3">
+            </use>
+        </svg>
+                `;
+    
+
+    playBtn.innerHTML = "";
+    playBtn.insertAdjacentHTML("afterbegin",markup)
+}
+
+renderPlayBtn();
+
+function playSong(){
+    renderPauseBtn();
+    music.play();
+}
+function pauseSong(){
+    renderPlayBtn();
+    music.pause();
+
+}
+
+function playNextSong(){
+    index++;
+    if(index > songTitles.length - 1){
+        index = 0;
+        renderSong(songTitles[index],artists[index]);
+        
+    }else{
+        renderSong(songTitles[index],artists[index]);
+    }
+    playSong();
+
+}
+
+
+function playPreviousSong(){
+    index--;
+    if(index < 0){
+        index = songTitles.length -1;
+        renderSong(songTitles[index],artists[index]);
+    }else{
+        renderSong(songTitles[index],artists[index]);
+    }
+    playSong();
+
+}
+
+function setCurrentProgress(){
+
+}
+
+
+
+function updatePlayProgress(e){
+    // console.log(e.srcElement)
+    const {duration , currentTime} = e.srcElement;
+
+    // console.log(duration , currentTime);
+    progressBar.style.width = `${(currentTime / duration) * 100}%`;
+
+}
+
+
+
+let pauseRendered = false;
+
+////////  EVENT LISTENERS;
+progressBarContainer.addEventListener("click",(e) => {
+    // console.log(e.offsetX);
+    // console.log(e.srcElement.clientWidth);
+    const position = e.offsetX 
+    const width = e.srcElement.clientWidth;
+    const p = (position / width)
+    music.currentTime = p * music.duration;
+    // console.log(music.currentTime)
+})
+nextBtn.addEventListener("click",playNextSong);
+
+prevBtn.addEventListener("click",playPreviousSong);
+
+
+
+playBtn.addEventListener("click",() => {
+    playBtn.classList.toggle("play");
+
+    const musicPlaying = playBtn.classList.contains("play");
+    // const musicNotPlaying = playBtn.className === "music-btn__play";
+    
+    if(musicPlaying){
+        playSong();
+    }else{
+        pauseSong();
+    }
+})
 
 //nav button not yet full fixed
 openMenuBtn.addEventListener("click",()=>{
-    // console.log(e.target)
     body.classList.toggle("show-nav");
-    if(!isCloseRendered) { 
-        renderCloseIcon();
-        isCloseRendered = true;
-    }
     
-    // console.log(!isCloseRendered);
 
     
 })
 
-// if(isCloseRendered){
-//     const markup = `<use  xlink:href ="./icons/sprite.svg#icon-menu"></use>`
-//     menuIcon.innerHTML =" ";
-//     menuIcon.insertAdjacentHTML("afterbegin",markup);
 
-//     isCloseRendered = false;
+closeMenuBtn.addEventListener("click",()=>{
+    body.classList.remove("show-nav");
+
+})
 
 
-// }
+
+music.addEventListener("timeupdate",updatePlayProgress)
+music.addEventListener("ended",playNextSong)
